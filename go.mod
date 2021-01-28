951fc13fe11368e8ff137f385341da5f9304ab57
Rysma  Aditya <rysmaaditya@Rysmas-MacBook-Air.local>
2021-01-28 09:07:42 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.15
+
+require rsc.io/quote v1.5.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/m/hello"
+)
+
+func main() {
+	fmt.Println(hello.Hello())
+}
