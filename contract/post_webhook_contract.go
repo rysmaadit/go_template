489d1d44b4956cf8aa69fe7f1a2cf4d30f4f4c90
rysmawidjaja <rysmawidjaja@gmail.com>
2021-02-08 22:11:40 +0700

    add post webhook request contract

---
+++ contract/post_webhook_contract.go
@@ -0,0 +1,27 @@
+package contract
+
+import (
+	"encoding/json"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type PostWebhookContract struct {
+	Date string `json:"date"`
+	SourceOrder string `json:"source_order"`
+	OrderID string `json:"order_id"`
+	InvoiceCode string `json:"invoice_code"`
+	CustomerName string `json:"customer_name"`
+	RecipientNumber string `json:"recipient_number"`
+	RecipientName string `json:"recipient_name"`
+}
+func NewPostWebhookRequest(r *http.Request) (*PostWebhookContract, error) {
+	var postWebhookContract PostWebhookContract
+
+	if err := json.NewDecoder(r.Body).Decode(&postWebhookContract); err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return &postWebhookContract, nil
+}
